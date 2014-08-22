850942d6769a51041690c62b47a56d5c25224108
kenwilcox <wilcoxk@gmail.com>
2014-08-21 20:01:41 -0600

    filteredls

---
+++ 04-filteredls/program.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function(err, files) {
+  if (!err) {
+  	files.forEach(function(file) {
+  	  if (path.extname(file) === "." + process.argv[3]) {
+        console.log(file);
+      }
+  	});
+  } else {
+    console.log(err);
+  }
+});
