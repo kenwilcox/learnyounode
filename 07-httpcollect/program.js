094ee876cd4a5b593fbf5b056547ebbdb6a4acbe
kenwilcox <wilcoxk@gmail.com>
2014-08-21 20:03:34 -0600

    httpcollect

---
+++ 07-httpcollect/program.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var bl = require('bl');
+
+http.get(process.argv[2], function (response) {
+	response.pipe(bl(function (err, data) {
+		if (!err) {
+			data = data.toString();
+			console.log(data.length);
+			console.log(data);
+		} else {
+			return console.error(err);
+		}
+	}));
+});
